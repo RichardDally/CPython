def32f9c62d34705241f5df05c4045d277357423
Richard Dally <r.dally@protonmail.com>
2020-04-16 20:36:44 +0200

    Add build_container shell instructions

---
+++ build_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Pick a version from https://www.python.org/ftp/python/
+PYTHON_MAJOR=3
+PYTHON_MINOR=7
+PYTHON_MICRO=6
+
+# Pick a tag from https://hub.docker.com/_/ubuntu?tab=description
+UBUNTU_VERSION=18.09
+
+PYTHON_VERSION=$PYTHON_MAJOR.$PYTHON_MINOR.$PYTHON_MICRO
+VERSION=$PYTHON_VERSION\_$UBUNTU_VERSION
+
+echo Building CPython $PYTHON_VERSION based on Ubuntu $VERSION
+
+sed -i "s/FROM ubuntu:18.04/FROM ubuntu:$UBUNTU_VERSION/g" Dockerfile
+#docker build . -t richarddally/cpython:$PYTHON_MAJOR.$PYTHON_MINOR.$PYTHON_MICRO --build-arg PYTHON_MAJOR=$PYTHON_MAJOR --build-arg PYTHON_MINOR=$PYTHON_MINOR --build-arg PYTHON_MICRO=$PYTHON_MICRO#"
